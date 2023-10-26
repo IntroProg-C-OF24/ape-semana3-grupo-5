49f97103e41489df6f16700a45917707ff139c14
Manuel <machavez35@utpl.edu.ec>
2023-10-26 17:55:57 -0500

    ESTO DE HA REALISADO

---
+++ ejercisio04/ejercisio04/src/main/java/com/mycompany/ejercisio04/Ejercisio04.java
@@ -0,0 +1,14 @@
+package com.mycompany.ejercisio04;
+public class Ejercisio04 {
+
+    public static void main(String[] args) {
+        int var1 = 20;
+        int var2 = 1;
+        int var3 = 3;
+        int var4 = 2;
+        int respuesta = ((var1)/(var2+var3))+ (var4); 
+        boolean reslutadofinal = respuesta >= var3;
+             System.out.println(reslutadofinal);
+        //ESTO ESTA REALISADO x2
+    }
+}
