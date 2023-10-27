f8a09f68be4444edf141d8082c96b563ae96d2c0
Alejandro_Jiron <jajiron1@utpl.edu.ec>
2023-10-27 11:27:39 -0500

    TERCER EJERCICIO REALIZADO Y RESUELTO

---
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -0,0 +1,17 @@
+package ejercicio03;
+public class Ejercicio03 {
+    public static void main(String[] args) {
+        int varl1 = 10;
+        int varl2 = 2;
+        int varl3 = 16;
+        int varl4 = 20;
+        int varl5 = 1;
+        int varl6 = 11;
+        //FORMLA A CONTINUACION 
+        double resultado = Math.pow(varl1, varl2) + Math.sqrt(varl3) + (double)varl4/(varl5 + varl6);
+        System.out.println("El resultado es: " + resultado);
+        //CORREGIDO Y REALIZADO POR ALEJANDRO JIRON.
+        
+    }
+    
+}
