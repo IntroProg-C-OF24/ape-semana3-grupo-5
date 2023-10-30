a18c399598ff887807ed19e3c1366719d3dab4a7
Alejandro_Jiron <jajiron1@utpl.edu.ec>
2023-10-30 18:41:44 -0500

    EJERCICIO 6 REALIZADO Y RESUELTO

--- Ejercicio04/ejercisio04/src/main/java/com/mycompany/ejercisio04/Ejercisio04.java
+++ Ejercicio04/ejercisio04/src/main/java/com/mycompany/ejercisio04/Ejercisio04.java
@@ -6,7 +6,7 @@ public class Ejercisio04 {
         int var2 = 1;
         int var3 = 3;
         int var4 = 2;
-        double respuesta = (double)var1/(var2 + var3) + var4;
+        double respuesta = var1/(var2 + var3) + var4;
              System.out.println("LA RESPUESTA ES:" + respuesta);
         //ESTO ESTA REALISADO x2
     }
