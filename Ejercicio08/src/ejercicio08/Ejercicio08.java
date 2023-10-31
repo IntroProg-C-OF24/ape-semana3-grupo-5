a1462a882a28f5a21cc7aeed7a380c3214776acf
Alejandro_Jiron <jajiron1@utpl.edu.ec>
2023-10-30 19:03:16 -0500

    EJERCICIO 09 REALIZADO Y RESUELTO

--- Ejercicio08/src/ejercicio08/Ejercicio08.java
+++ Ejercicio08/src/ejercicio08/Ejercicio08.java
@@ -13,12 +13,10 @@ public class Ejercicio08 {
         double var10 = ((var1 * var2) + var3 - (var4 / var5) + Math.pow(var6, 2));
         double var11 = (var8 - var8 + var9);
         boolean resultado1 = var10 >= var7;
-        //Comparamos las dos cantidades y obtenemos un valor de verdad.
         boolean resultado2 = var8 >= var11;
-        //Comparamos las dos cantidades y obtenemos otro valor de verdad.
         boolean resultadoFinal = resultado1 || resultado2;
-        //Operamos los dos booleanos con el conector logico OR y obtenemos el resultado final.
         System.out.println("La operacion da como resultado: " + resultadoFinal);
+        //EJERCICIO 08 REALIZADO Y RESUELTO.
     }
     
 }
